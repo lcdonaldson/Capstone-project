0982139e016db7c392b44d0ab88a0a5e4d94cba1
Levi Donaldson <lcdbacanas@gmail.com>
2015-08-25 15:24:51 -0700

    deleted models and collections

--- Collections/rooms.js
+++
@@ -1,7 +0,0 @@
-
-var RoomCollection = Backbone.Collection.extend({
-    // model: App.Models.Room,
-    model: RoomModel,
-    url: 'http://localhost:3000/rooms'
-})
-// App.Collections.Rooms = RoomCollection
--- Collections/users.js
+++
@@ -1,7 +0,0 @@
-
-var UserCollection = Backbone.Collection.extend({
-    // model: App.Models.User
-    model: UserModel,
-    url: 'http://localhost:3000/users'
-})
-// App.Collections.Users = UserCollection
